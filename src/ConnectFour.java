8171f1e0c4f700ec52a5cb49aa70dca8ec99c706
Oksana-mac <ms.oksana@mail.ru>
2021-05-28 13:38:08 +0300

    Connect Four Game

--- src/ConnectFour.java
+++ src/ConnectFour.java
@@ -1,30 +1,33 @@
-import javax.sound.midi.Soundbank;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class ConnectFour {
 
     private static final char[] PLAYERS = {'R', 'G'};
-    private int boardWidth;
-    private int boardHeight;
+    private final int boardHeight;
     private final char[][] grid;
+    private static final char[][] arr = new char[6][7];
     private int lastTop = -1, lastCol = -1;
 
-    public ConnectFour (int boardWidth, int boardHeight) {
+    public ConnectFour(int boardWidth, int boardHeight) {
         this.boardHeight = boardHeight;
-        this.boardWidth = boardWidth;
-        grid = new char[boardHeight][];
+        grid = new char[boardHeight][boardWidth];
 
         for (int i = 0; i < boardHeight; i++) {
-            Arrays.fill(grid[i] = new char[boardWidth], '.');
+            for (int j = 0; j < boardWidth; j++) {
+                if (j % 2 == 0) {
+                    grid[i][j] = '|';
+                } else {
+                    grid[i][j] = ' ';
+                }
+
+            }
         }
     }
 
     public String toString() {
-        return IntStream.range(0 , boardWidth).mapToObj(Integer::toString).collect(Collectors.joining()) + "\n"
-                + Arrays.stream(grid).map(String::new).collect(Collectors.joining("\n"));
+        return Arrays.stream(grid).map(String::new).collect(Collectors.joining("\n"));
     }
 
     public String horizontal() {
@@ -35,79 +38,132 @@ public class ConnectFour {
         StringBuilder sb = new StringBuilder(boardHeight);
 
         for (int i = 0; i < boardHeight; i++) {
-            sb.append(grid[boardHeight][lastCol]);
+            sb.append(grid[i][lastCol]);
         }
         return sb.toString();
     }
 
-    public String slashDioganal() {
-        StringBuilder sb = new StringBuilder(boardHeight);
-        for (int i = 0; i < boardHeight; i++) {
-            int w = lastCol + lastTop - i;
+    public boolean checkDiaganal() {
+        boolean res;
+        int cl;
+        int cr;
+        int r;
+        for (r = 5; 3 <= r; r--) {
+            // Проверяем диагональ слева
+            for (cl = 0; cl + 3 < 7; cl++) {
+                if (arr[r][cl] != '\u0000' || arr[r][cl] != 0) {
+                    res = (arr[r][cl] == arr[r - 1][cl + 1]) &&
+                            (arr[r][cl] == arr[r - 2][cl + 2]) &&
+                            (arr[r][cl] == arr[r - 3][cl + 3]);
 
-            if (0 <= w && w < boardWidth) {
-                sb.append(grid[i][w]);
+                    if (res) {
+                        return true;
+                    }
+                }
+            }
+
+            for (cr = 6; 0 <= cr - 3; cr--) {
+                if (arr[r][cr] != '\u0000' || arr[r][cr] != 0) {
+                    res = (arr[r][cr] == arr[r - 1][cr - 1]) &&
+                            (arr[r][cr] == arr[r - 2][cr - 2]) &&
+                            (arr[r][cr] == arr[r - 3][cr - 3]);
+
+                    if (res) {
+                        return true;
+                    }
+                }
             }
         }
-        return sb.toString();
+        return false;
     }
 
-    public String backlashDiagonal() {
-        StringBuilder sb = new StringBuilder(boardHeight);
-        for (int i = 0; i < boardHeight; i++) {
-            int w = lastCol - lastTop + i;
-
-            if (0 <= w && w < boardWidth) {
-                sb.append(grid[i][w]);
-            }
-        }
-        return sb.toString();
+    public static boolean contains(String str, String substring) {
+        return str.contains(substring);
     }
 
-    public  static  boolean contains(String str, String substring) {
-        return str.indexOf(substring) >= 0;
-    }
-
-    public  boolean isWinningPlay() {
-        if(lastCol  == -1) {
+    public boolean isWinningPlay() {
+        int fill = 0;
+        if (lastCol == -1) {
             System.err.println("No movie");
             return false;
         }
+        for (int r = 0; r < 6; r++) {
+            if (arr[0][r] != 0 && arr[0][r] != '\u0000') {
+                fill++;
+            }
+            if (fill == 6) {
+                System.out.println("Game over! draw!");
+                break;
+            }
+        }
         char sym = grid[lastTop][lastCol];
-        String streak = String.format("%c%c%c%c", sym, sym, sym, sym);
-
-        return contains(horizontal(), streak) ||contains(vertical(), streak)
-                || contains(slashDioganal(), streak) || contains(backlashDiagonal(), streak);
+        String streakG = String.format("|%c|%c|%c|%c|", sym, sym, sym, sym);
+        String streakV = String.format("%c%c%c%c", sym, sym, sym, sym);
+        return contains(horizontal(), streakG) || contains(vertical(), streakV);
     }
 
     public void chooseAndDrop(char symbol, Scanner input) {
         do {
-            System.out.println("\nPlayer " + symbol + "turn: ");
             int col = input.nextInt();
-            if (!(0<= col && col < boardWidth)) {
-                System.out.println("Column must be between 0 and " + (boardWidth-1));
+
+            if (!(0 < col && col <= 7)) {
+                System.out.println("Column must be between 1 - 7 ");
                 continue;
             }
+            String player;
+            if (symbol == 'R') {
+                player = "RED";
+                System.out.println("\nPlayer " + 1 + " [" + player + "] choose column(1-7): " + col);
 
-            for ( int i = boardHeight - 1; i >=0; i--) {
-                if(grid[i][col] == '.') {
+            } else {
+                player = "GREEN";
+                System.out.println("\nPlayer " + 2 + " [" + player + "] choose column(1-7) " + col);
+            }
 
-                    grid[lastTop = i][lastCol = col] = symbol;
+            int n = 0;
+            for (int i = boardHeight - 1; i >= 0; i--) {
+                switch (col) {
+                    case 1:
+                        n = 1;
+                        break;
+                    case 2:
+                        n = 3;
+                        break;
+                    case 3:
+                        n = 5;
+                        break;
+                    case 4:
+                        n = 7;
+                        break;
+                    case 5:
+                        n = 9;
+                        break;
+                    case 6:
+                        n = 11;
+                        break;
+                    case 7:
+                        n = 13;
+                        break;
+                }
+                if (grid[i][n] == ' ') {
+                    grid[lastTop = i][lastCol = n] = symbol;
+                    arr[i][col - 1] = symbol;
                     return;
                 }
             }
             System.out.println("Column " + col + "is full.");
-        } while (true);
+        }
+        while (true);
     }
 
     public static void main(String[] args) {
         try (Scanner input = new Scanner((System.in))) {
-            int height = 6, wight = 7;
+            int height = 6, wight = 15;
             int moves = height * wight;
 
             ConnectFour board = new ConnectFour(wight, height);
 
-            System.out.println("Use 0-" + (wight-1) + "to choose a column");
+            System.out.println("Use 1-7 to choose a column");
             System.out.println(board);
 
             for (int player = 0; moves-- > 0; player = 1 - player) {
@@ -115,11 +171,12 @@ public class ConnectFour {
                 board.chooseAndDrop(symbol, input);
                 System.out.println(board);
 
-                if (board.isWinningPlay()) {
-                    System.out.println("\nPlayer " + symbol + "wins!");
+                if (board.isWinningPlay() || board.checkDiaganal()) {
+                    System.out.println("\nPlayer " + symbol + " wins!");
+                    break;
                 }
             }
-            System.out.println( "Game over");
+            System.out.println("Game over");
         }
     }
 }
